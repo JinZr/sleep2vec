c371af46d5bb4dcabe3432f611c385832af1f25b
zr_jin <peter.jin.cn@gmail.com>
2026-05-07 10:07:45 +0800

    Sync plot pattern updates to sleep2expert

--- tests/test_pair_acc_visualization.py
+++ tests/test_pair_acc_visualization.py
@@ -1,27 +1,21 @@
 from matplotlib.patches import FancyBboxPatch
 import matplotlib.pyplot as plt
 import numpy as np
+import pytest
 
 from sleep2expert.visualization.pair_acc import render_pair_acc_heatmap as render_sleep2expert_pair_acc_heatmap
 from sleep2vec2.visualization.pair_acc import render_pair_acc_heatmap as render_sleep2vec2_pair_acc_heatmap
 from sleep2vec.visualization.pair_acc import render_pair_acc_heatmap
 
 
-def test_render_pair_acc_heatmap_uses_non_overlapping_vertical_x_labels():
+def test_render_pair_acc_heatmap_uses_tilted_x_labels():
     matrix = np.zeros((3, 3), dtype=np.float32)
     fig = render_pair_acc_heatmap(matrix, ["heartbeat", "eeg_original", "resp_nasal_original"])
     try:
         labels = fig.axes[0].get_xticklabels()
-        assert [label.get_rotation() for label in labels] == [90.0, 90.0, 90.0]
-        assert [label.get_ha() for label in labels] == ["center", "center", "center"]
-        assert [label.get_va() for label in labels] == ["top", "top", "top"]
-        fig.canvas.draw()
-        renderer = fig.canvas.get_renderer()
-        tick_boxes = [label.get_window_extent(renderer=renderer) for label in labels]
-        heatmap_bottom = fig.axes[0].get_window_extent(renderer=renderer).y0
-        xlabel_top = fig.axes[0].xaxis.label.get_window_extent(renderer=renderer).y1
-        assert max(box.y1 for box in tick_boxes) <= heatmap_bottom
-        assert xlabel_top <= min(box.y0 for box in tick_boxes)
+        assert [label.get_rotation() for label in labels] == [45.0, 45.0, 45.0]
+        assert [label.get_ha() for label in labels] == ["right", "right", "right"]
+        assert [label.get_rotation_mode() for label in labels] == ["anchor", "anchor", "anchor"]
     finally:
         plt.close(fig)
 
@@ -45,28 +39,22 @@ def test_render_sleep2vec2_pair_acc_heatmap_uses_non_overlapping_vertical_x_labe
         plt.close(fig)
 
 
-def test_render_sleep2expert_pair_acc_heatmap_uses_non_overlapping_vertical_x_labels():
+def test_render_sleep2expert_pair_acc_heatmap_uses_tilted_x_labels():
     matrix = np.zeros((3, 3), dtype=np.float32)
     fig = render_sleep2expert_pair_acc_heatmap(matrix, ["heartbeat", "eeg_original", "resp_nasal_original"])
     try:
         labels = fig.axes[0].get_xticklabels()
-        assert [label.get_rotation() for label in labels] == [90.0, 90.0, 90.0]
-        assert [label.get_ha() for label in labels] == ["center", "center", "center"]
-        assert [label.get_va() for label in labels] == ["top", "top", "top"]
-        fig.canvas.draw()
-        renderer = fig.canvas.get_renderer()
-        tick_boxes = [label.get_window_extent(renderer=renderer) for label in labels]
-        heatmap_bottom = fig.axes[0].get_window_extent(renderer=renderer).y0
-        xlabel_top = fig.axes[0].xaxis.label.get_window_extent(renderer=renderer).y1
-        assert max(box.y1 for box in tick_boxes) <= heatmap_bottom
-        assert xlabel_top <= min(box.y0 for box in tick_boxes)
+        assert [label.get_rotation() for label in labels] == [45.0, 45.0, 45.0]
+        assert [label.get_ha() for label in labels] == ["right", "right", "right"]
+        assert [label.get_rotation_mode() for label in labels] == ["anchor", "anchor", "anchor"]
     finally:
         plt.close(fig)
 
 
-def test_render_pair_acc_heatmap_draws_full_axis_title_boxes_clear_of_long_labels():
+@pytest.mark.parametrize("render_heatmap", [render_pair_acc_heatmap, render_sleep2expert_pair_acc_heatmap])
+def test_render_pair_acc_heatmap_draws_full_axis_title_boxes_clear_of_long_labels(render_heatmap):
     matrix = np.zeros((2, 2), dtype=np.float32)
-    fig = render_pair_acc_heatmap(matrix, ["very-long-heartbeat-label", "very-long-respiration-label"])
+    fig = render_heatmap(matrix, ["very-long-heartbeat-label", "very-long-respiration-label"])
 
     try:
         ax = fig.axes[0]
